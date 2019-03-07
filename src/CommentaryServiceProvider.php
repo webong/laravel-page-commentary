bc561212c9dbad469bd3a6c1e1d522809741cbff
wisdomanthoni <wizitendo10@gmail.com>
2019-03-07 13:06:59 +0100

    update service provider class

--- src/CommentaryServiceProvider.php
+++ src/CommentaryServiceProvider.php
@@ -13,15 +13,12 @@ class CommentaryServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'creativitykills');
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'creativitykills');
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
         // Publishing is only necessary when using the CLI.
         if ($this->app->runningInConsole()) {
             $this->bootForConsole();
         }
+
+        $this->loadViewsFrom(__DIR__.'/../resources/views', 'creativitykills');
     }
 
     /**
@@ -62,21 +59,17 @@ class CommentaryServiceProvider extends ServiceProvider
         ], 'commentary.config');
 
         // Publishing the views.
-        /*$this->publishes([
+        $this->publishes([
             __DIR__.'/../resources/views' => base_path('resources/views/vendor/creativitykills'),
-        ], 'commentary.views');*/
+        ], 'commentary.views');
 
-        // Publishing assets.
-        /*$this->publishes([
-            __DIR__.'/../resources/assets' => public_path('vendor/creativitykills'),
-        ], 'commentary.views');*/
+        // Publishing public assets.
+        $this->publishes([
+            __DIR__.'/../public' => public_path('vendor/creativitykills'),
+        ], 'commentary.views');
 
-        // Publishing the translation files.
-        /*$this->publishes([
-            __DIR__.'/../resources/lang' => resource_path('lang/vendor/creativitykills'),
-        ], 'commentary.views');*/
-
-        // Registering package commands.
-        // $this->commands([]);
+        $this->publishes([
+            __DIR__ . '/../database/migrations/' => database_path('migrations'),
+        ], 'migrations');
     }
 }
